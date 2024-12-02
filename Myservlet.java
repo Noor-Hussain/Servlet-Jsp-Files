95addeac7374f432f73f9d084718b74245c9f00b
Noor-Hussain <romiyoromi255@gmail.com>
2024-12-01 20:04:04 -0800

    WebAppWithoutXmlFIle
    Web Application without xml file

--- Myservlet.java
+++ Myservlet.java
@@ -1,21 +1,20 @@
-package com.mybackendservlet;
+package in.comp;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class Myservlet extends HttpServlet {
-	@Override
- protected void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException,IOException
- {
-		PrintWriter out = resp.getWriter();
-		out.print("i am in do get method for browser");
-	System.out.println("i am in do get doGet() method");
+@WebServlet("/aaa")
+public class Myservlet extends HttpServlet{
 	
- }
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp)throws ServletException,IOException 
+	{
+		System.out.println("i am in my Myservlet  qservice method");
+	}
 
 }
